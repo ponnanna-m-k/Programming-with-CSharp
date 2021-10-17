7b90af83f51a0e0c476299df32eee5cd42b49b32
ponnanna-m-k <01xenon01@gmail.com>
2021-10-17 16:49:48 +0530

    Files Uploaded

--- Day 4/TextToSpeech/TextToSpeech/Form1.Designer.cs
+++ Day 4/TextToSpeech/TextToSpeech/Form1.Designer.cs
@@ -36,18 +36,20 @@ namespace TextToSpeech
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(235, 127);
+            this.textBox1.Location = new System.Drawing.Point(348, 300);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(334, 20);
+            this.textBox1.Size = new System.Drawing.Size(499, 26);
             this.textBox1.TabIndex = 0;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(370, 198);
+            this.button1.Location = new System.Drawing.Point(514, 381);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(166, 41);
             this.button1.TabIndex = 1;
             this.button1.Text = "Convert";
             this.button1.UseVisualStyleBackColor = true;
@@ -57,21 +59,24 @@ namespace TextToSpeech
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(323, 70);
+            this.label1.Location = new System.Drawing.Point(480, 213);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(158, 18);
+            this.label1.Size = new System.Drawing.Size(222, 27);
             this.label1.TabIndex = 2;
             this.label1.Text = "Enter your Text";
             this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(1200, 692);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
