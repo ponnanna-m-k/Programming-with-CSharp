9bccd4cb14458833f6c0aa81572ed1ce63a9ef18
ponnanna-m-k <01xenon01@gmail.com>
2021-10-18 15:47:18 +0530

    Day 5

---
+++ Day 5/DestructorDemo/DestructorDemo/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DestructorDemo
+{
+    public class Class1 : IDisposable
+    {
+        public void Display_Msg()
+        {
+            Console.WriteLine("Inside Display Method");
+
+
+
+        }
+        public Class1()
+        {
+            Console.WriteLine("Default Constructor");
+        }
+        public Class1(string s)
+        {
+            Console.WriteLine("Parameterized Constructor with a message {0}", s);
+        }
+        public void Dispose()
+        {
+            Console.WriteLine("Freeing up space using Dispose");
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ Day 5/DestructorDemo/DestructorDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DestructorDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Finalizer!");
+            Class1 obj = new Class1("Helloo ");
+            obj.Display_Msg();
+            obj.Dispose();
+            Console.ReadKey();
+        }
+    }
+}
