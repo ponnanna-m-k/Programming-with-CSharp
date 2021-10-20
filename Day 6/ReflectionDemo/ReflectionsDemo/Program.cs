252d0467e1f96014d417eb924d29d821b344d7be
ponnanna-m-k <01xenon01@gmail.com>
2021-10-20 14:32:48 +0530

    Day 6

--- Day 6/ReflectionDemo/ReflectionsDemo/Program.cs
+++ Day 6/ReflectionDemo/ReflectionsDemo/Program.cs
@@ -7,13 +7,13 @@ namespace ReflectionDemo
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Reflection Demo in C#....");
-            //Type T = typeof(int);
-            //Console.WriteLine("Type Of Name : {0}", T.Name);
-            //Console.WriteLine("Full name of the type defined : {0}", T.FullName);
-            //Console.WriteLine("Type Of Assembly : {0} ", T.Assembly);
-            //Console.WriteLine("Information about base type : {0}", T.BaseType);
-            //Console.WriteLine("Attributes of type : {0}",T.Attributes);
+            Console.WriteLine("Reflection Demo in C#....");
+            Type T = typeof(int);
+            Console.WriteLine("Type Of Name : {0}", T.Name);
+            Console.WriteLine("Full name of the type defined : {0}", T.FullName);
+            Console.WriteLine("Type Of Assembly : {0} ", T.Assembly);
+            Console.WriteLine("Information about base type : {0}", T.BaseType);
+            Console.WriteLine("Attributes of type : {0}", T.Attributes);
 
 
             //Demonstration of dynamic objects
